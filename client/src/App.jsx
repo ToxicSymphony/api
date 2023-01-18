85b46ed3d0d4a6e700debc2824adb0b452bfee09
jaimezzapata <zapataval2304@GMAIL.COM>
2023-01-18 06:49:19 -0500

    Se modifica el endpoint de errores

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,15 +1,17 @@
 import Listado from "./components/productos/Listado"
 import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+import Error404 from "./components/helpers/Error404"
 
 const router = createBrowserRouter([
   {
     path: '/productos',
     element: <Listado />,
-    errorElement: <h1>Erro404</h1>
+    errorElement: <Error404 />
   },
   {
     path: '/',
-    element: <h1>Home</h1>
+    element: <h1>Home</h1>,
+    errorElement: <Error404 />
   }
 ])
 
