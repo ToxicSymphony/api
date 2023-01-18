bdc7e23048e19bec7034d1ffa5e003810b381879
jaimezzapata <zapataval2304@GMAIL.COM>
2023-01-18 06:37:02 -0500

    Se configura las rutas del front

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,22 @@
 import Listado from "./components/productos/Listado"
+import {createBrowserRouter, RouterProvider} from 'react-router-dom'
+
+const router = createBrowserRouter([
+  {
+    path: '/productos',
+    element: <Listado />,
+    errorElement: <h1>Erro404</h1>
+  },
+  {
+    path: '/',
+    element: <h1>Home</h1>
+  }
+])
 
 function App() {
   return (
     <div className="App">
-     <Listado />
+     <RouterProvider router={router} />
     </div>
   )
 }
