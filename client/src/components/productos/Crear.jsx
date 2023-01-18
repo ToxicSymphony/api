6d6c54cc89f97e5b1f95a2720760697e6c07b97a
jaimezzapata <zapataval2304@GMAIL.COM>
2023-01-18 08:04:14 -0500

    Se crea el metodo POST

--- client/src/components/productos/Crear.jsx
+++ client/src/components/productos/Crear.jsx
@@ -1,39 +1,106 @@
 import React from "react";
-import {Link} from 'react-router-dom'
-import axios from 'axios'
+import { Link, useNavigate } from "react-router-dom";
+import axios from "axios";
 import { useState } from "react";
-const urlProductos = 'http://localhost:3100/productos'
+const urlProductos = "http://localhost:3100/productos";
 
 const Crear = () => {
-  const [referencia, setReferencia] = useState('')
-  const [nombre, setNombre] = useState('')
-  const [descripcion, setDescripcion] = useState('')
-  const [cantidad, setCantidad] = useState('')
-  const [valor, setValor] = useState('')
-  const [categoria, setCategoria] = useState('')
-  const [gama, setGama] = useState('')
-  const [imagen, setImagen] = useState('')
+  const [referencia, setReferencia] = useState("");
+  const [nombre, setNombre] = useState("");
+  const [descripcion, setDescripcion] = useState("");
+  const [cantidad, setCantidad] = useState("");
+  const [valor, setValor] = useState("");
+  const [categoria, setCategoria] = useState("");
+  const [gama, setGama] = useState("");
+  const [imagen, setImagen] = useState("");
+  const navigate = useNavigate();
+
+  const crearProducto = async (e) => {
+    e.preventDafault()
+    producto = {
+      referencia,
+      nombre,
+      descripcion,
+      cantidad,
+      valor,
+      categoria,
+      gama,
+      imagen,
+    };
+    await axios.post(urlProductos, producto);
+    navigate('/productos')
+  };
 
   return (
     <section className="container bg-warning p-5">
-      <form className="form-control p-5" action="">
+      <form className="form-control p-5" action="" method="">
         <section className="row">
           <section className="col">
-            <input onChange={(e)=>setReferencia(e.target.value)} value={referencia} placeholder="Referencia" className="form-control" type="text" />
-            <input onChange={(e)=>setNombre(e.target.value)} value={nombre} placeholder="Nombre" className="form-control" type="text" />
-            <input onChange={(e)=>setDescripcion(e.target.value)} value={descripcion} placeholder="Descripcion" className="form-control" type="text" />
-            <input onChange={(e)=>setValor(e.target.value)} value={cantidad} placeholder="Cantidad" className="form-control" type="text" />
+            <input
+              onChange={(e) => setReferencia(e.target.value)}
+              value={referencia}
+              placeholder="Referencia"
+              className="form-control"
+              type="text"
+            />
+            <input
+              onChange={(e) => setNombre(e.target.value)}
+              value={nombre}
+              placeholder="Nombre"
+              className="form-control"
+              type="text"
+            />
+            <input
+              onChange={(e) => setDescripcion(e.target.value)}
+              value={descripcion}
+              placeholder="Descripcion"
+              className="form-control"
+              type="text"
+            />
+            <input
+              onChange={(e) => setCantidad(e.target.value)}
+              value={cantidad}
+              placeholder="Cantidad"
+              className="form-control"
+              type="text"
+            />
           </section>
           <section className="col">
-            <input onChange={(e)=>setCantidad(e.target.value)} value={valor} placeholder="Valor" className="form-control" type="text" />
-            <input onChange={(e)=>setCategoria(e.target.value)} value={categoria} placeholder="Categoria" className="form-control" type="text" />
-            <input onChange={(e)=>setGama(e.target.value)}value={gama} placeholder="Gama" className="form-control" type="text" />
-            <input onChange={(e)=>setImagen(e.target.value)} value={imagen} placeholder="Imagen" className="form-control" type="text" />
+            <input
+              onChange={(e) => setValor(e.target.value)}
+              value={valor}
+              placeholder="Valor"
+              className="form-control"
+              type="text"
+            />
+            <input
+              onChange={(e) => setCategoria(e.target.value)}
+              value={categoria}
+              placeholder="Categoria"
+              className="form-control"
+              type="text"
+            />
+            <input
+              onChange={(e) => setGama(e.target.value)}
+              value={gama}
+              placeholder="Gama"
+              className="form-control"
+              type="text"
+            />
+            <input
+              onChange={(e) => setImagen(e.target.value)}
+              value={imagen}
+              placeholder="Imagen"
+              className="form-control"
+              type="text"
+            />
           </section>
         </section>
         <section className="row">
-          <Link to={'/productos'} className="btn btn-danger form-control" >Cancelar</Link>
-          <button className="btn btn-success form-control" >Guardar</button>
+          <Link to={"/productos"} className="btn btn-danger form-control">
+            Cancelar
+          </Link>
+          <button onClick={crearProducto} className="btn btn-success form-control">Guardar</button>
         </section>
       </form>
     </section>
